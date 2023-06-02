c4ed2ab2e6a294544b5ee1698281d1db9078fae2
David Gasquez <davidgasquez@gmail.com>
2023-06-02 14:31:35 +0000

    chore: manage R2 bucket

--- main.tf
+++ main.tf
@@ -1,7 +1,16 @@
 terraform {
   required_providers {
-    github = {
-      source = "integrations/github"
+    cloudflare = {
+      source = "cloudflare/cloudflare"
     }
   }
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+resource "cloudflare_r2_bucket" "sandbox" {
+  account_id = "fbe814d4a37d3d3d7fcfeec0ab929ff6"
+  name       = "sandbox"
 }
---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "bucket_id" {
+  value = cloudflare_r2_bucket.sandbox.id
+}
+
+output "bucket_name" {
+  value = cloudflare_r2_bucket.sandbox.name
+}
